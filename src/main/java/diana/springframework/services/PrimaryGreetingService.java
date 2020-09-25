a5667bdadf02ea6a9c980a13bb08c9675188cc41
Dya92 <45461244+Dya92@users.noreply.github.com>
2020-09-25 16:49:40 +0300

    Update PrimaryGreetingService.java

--- src/main/java/diana/springframework/services/PrimaryGreetingService.java
+++ src/main/java/diana/springframework/services/PrimaryGreetingService.java
@@ -6,10 +6,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en","default"})
-public class PrimaryGreetingService implements GreetingService{
+@Profile({"en", "default"})
+public class PrimaryGreetingService implements GreetingService {
+
+    private GreetingRepository greetingRepository;
+
+    public PrimaryGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
-        return "Hello - from the primary greeting service";
+        return greetingRepository.getEnglishGreeting();
     }
 }
